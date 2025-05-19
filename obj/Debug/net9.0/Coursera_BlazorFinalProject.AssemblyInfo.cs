069d62bfc91196b48b15ba738cec33273d70cb0c
Robert Palmer <Awriter247@gmail.com>
2025-05-18 22:49:31 -0400

    Updates CI workflow to build, test, and version Blazor Server project
    This commit modifies the CI workflow to build, test, and version a Blazor Server project. It introduces steps for building, testing, and setting the informational version of the project assembly.

--- obj/Debug/net9.0/Coursera_BlazorFinalProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/Coursera_BlazorFinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coursera_BlazorFinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40b214366684e9e7b5963a0ca5fdc9148827e10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211ac7a15510ac2ab33769ca50aef065b9a30ae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coursera_BlazorFinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coursera_BlazorFinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
